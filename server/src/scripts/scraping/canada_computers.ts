5cb033572c71c180c5860e3eedce955f62eeb1ae
Amandeep Rehal <arehal4@gmail.com>
2021-01-27 17:38:51 -0800

    feat: bulk scrape Canada Computers search results

--- server/src/scripts/scraping/canada_computers.ts
+++ server/src/scripts/scraping/canada_computers.ts
@@ -1,9 +1,19 @@
-import { By, ThenableWebDriver } from "selenium-webdriver";
+import { By, Key, ThenableWebDriver, until, WebElement } from "selenium-webdriver";
 
+const CANADA_COMPUTERS_URL =
+  "https://www.canadacomputers.com/search/results_details.php?language=en&keywords=";
 const TITLE_IDENTIFIER = "h3 mb-0";
 const PRICE_IDENTIFIER = "h2-big";
+const SEARCH_IDENTIFIER = "cc_quick_search";
+const PRODUCT_LIST_IDENTIFIER = "product-list";
+const RESULTS_LIST_IDENTIFIER = "toggleBox";
+const RESULTS_TITLE_IDENTIFIER = "productTemplate_title";
+const RESULTS_PRICE_INDENTIFIER = "pq-hdr-product_price";
+const IMAGE_TAG = "img";
 const IMAGE_SELECTOR = "slick-image";
 const IMAGE_ATTRIBUTE = "src";
+const ANCHOR_TAG = "a";
+const HREF_TAG = "href";
 
 // Nestbuy scraping item given url
 export const scrapeCanadaComputersUrl = (driver: ThenableWebDriver, URL: string) => {
@@ -18,6 +28,52 @@ export const scrapeCanadaComputersUrl = (driver: ThenableWebDriver, URL: string)
       const imageURL = await imageElement.getAttribute(IMAGE_ATTRIBUTE);
 
       return { title, price, URL, imageURL };
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const scrapeCanadaComputersSearch = async (driver: ThenableWebDriver, input: string) => {
+  //create search query string
+  const searchQuery = input.replace(" ", "+");
+
+  try {
+    // navigate to Newegg
+    return await driver.get(CANADA_COMPUTERS_URL + searchQuery).then(async () => {
+      // search for user inputted keyword
+      await driver.findElement(By.id(SEARCH_IDENTIFIER)).sendKeys(input);
+      await driver.findElement(By.id(SEARCH_IDENTIFIER)).sendKeys(Key.ENTER);
+
+      // wait for page to load
+      await driver.wait(until.elementLocated(By.id(PRODUCT_LIST_IDENTIFIER)), 5000);
+
+      return await driver
+        .findElements(By.className(RESULTS_LIST_IDENTIFIER))
+        .then(async (items: WebElement[]) => {
+          // scrape all search results for title and price
+          let results = items.map(async (item: WebElement) => {
+            const title: string = await item
+              .findElement(By.className(RESULTS_TITLE_IDENTIFIER))
+              .getText();
+
+            const priceText: string = await item
+              .findElement(By.className(RESULTS_PRICE_INDENTIFIER))
+              .getText();
+            const price: number = +priceText.split(" ")[0].substring(1).replace(",", "");
+
+            const URL: string = await (await item.findElement(By.css(ANCHOR_TAG))).getAttribute(
+              HREF_TAG
+            );
+
+            const imageElement = await item.findElement(By.css(IMAGE_TAG));
+            const imageURL = await imageElement.getAttribute(IMAGE_ATTRIBUTE);
+
+            return { title, price, URL, imageURL };
+          });
+
+          return Promise.all(results);
+        });
     });
   } catch (err) {
     console.error(err);
