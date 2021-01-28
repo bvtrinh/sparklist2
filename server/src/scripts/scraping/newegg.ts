7cc0a222f1b768645fe54b65d9aa8d1e7d8e29b0
Amandeep Rehal <arehal4@gmail.com>
2021-01-27 17:40:33 -0800

    feat: bulk scrape Newegg search results

--- server/src/scripts/scraping/newegg.ts
+++ server/src/scripts/scraping/newegg.ts
@@ -1,9 +1,16 @@
-import { By, ThenableWebDriver } from "selenium-webdriver";
+import { By, ThenableWebDriver, WebElement } from "selenium-webdriver";
 
+const NEWEGG_URL = "https://www.newegg.ca/p/pl?d=";
 const TITLE = "h1";
 const PRICE = "price-current";
+const SEARCH_RESULTS = "item-cell";
+const RESULT_TITLE = "item-title";
+const RESULT_PRICE = "price-current";
 const IMAGE_SELECTOR = "product-view-img-original";
+const IMAGE_TAG = "img";
 const IMAGE_ATTRIBUTE = "src";
+const ANCHOR_TAG = "a";
+const HREF_TAG = "href";
 
 // Newegg scraping item given url
 export const scrapeNeweggUrl = (driver: ThenableWebDriver, URL: string) => {
@@ -19,6 +26,41 @@ export const scrapeNeweggUrl = (driver: ThenableWebDriver, URL: string) => {
       const imageURL = await imageElement.getAttribute(IMAGE_ATTRIBUTE);
 
       return { title, price, URL, imageURL };
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const scrapeNeweggSearch = async (driver: ThenableWebDriver, input: string) => {
+  //create search query string
+  const searchQuery = input.replace(" ", "+");
+
+  try {
+    // navigate to Newegg
+    return await driver.get(NEWEGG_URL + searchQuery).then(async () => {
+      return await driver
+        .findElements(By.className(SEARCH_RESULTS))
+        .then(async (items: WebElement[]) => {
+          // scrape all search results for title and price
+          let results = items.map(async (item: WebElement) => {
+            const title: string = await item.findElement(By.className(RESULT_TITLE)).getText();
+
+            const priceText: string = await item.findElement(By.className(RESULT_PRICE)).getText();
+            const price: number = +priceText.split(" ")[0].substring(1);
+
+            const URL: string = await (await item.findElement(By.css(ANCHOR_TAG))).getAttribute(
+              HREF_TAG
+            );
+
+            const imageElement = await item.findElement(By.css(IMAGE_TAG));
+            const imageURL = await imageElement.getAttribute(IMAGE_ATTRIBUTE);
+
+            return { title, price, URL, imageURL };
+          });
+
+          return Promise.all(results);
+        });
     });
   } catch (err) {
     console.error(err);
