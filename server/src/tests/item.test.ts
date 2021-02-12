c2477b2458ccf1b8fecef1c127205e7a5ff4fcea
Amandeep Rehal <arehal4@gmail.com>
2021-02-11 19:58:49 -0800

    refactor: updated items controller tests

--- server/src/tests/item.test.ts
+++ server/src/tests/item.test.ts
@@ -1,37 +1,22 @@
 import { resolve } from "path";
 import { config } from "dotenv";
-config({ path: resolve(__dirname, "../../.env.development") });
+config({ path: resolve(__dirname, "../../.env.test") });
 import { connect, disconnect } from "../models/connect";
-import { updateItem } from "../controllers/item";
-// import { Item } from "../models/item.model";
+import { IItem } from "../models/item.model";
 
+// eslint-disable-next-line
 const app = require("../index");
-const supertest = require("supertest");
+import supertest from "supertest";
 const request = supertest(app);
 
-const itemData = {
-  title: "Sony Over-Ear Noise Cancelling Bluetooth Headphones (WH-1000XM4/B) - Black",
-  currentPrice: 499.99,
-  url: "https://www.bestbuy.ca/en-ca/product/14777258",
-  imageURL: "https://multimedia.bbycastatic.ca/multimedia/products/500x500/147/14777/14777258.jpg",
-};
+jest.setTimeout(15000);
 
-const updatedItemData = {
-  title: "Sony WH-1000XM4",
-  currentPrice: 349.99,
-  url: "https://www.bestbuy.ca/en-ca/product/14777258",
-  imageURL: "https://multimedia.bbycastatic.ca/multimedia/products/500x500/147/14777/14777258.jpg",
-};
+const itemURLs = [
+  { url: "https://www.bestbuy.ca/en-ca/product/14777258" },
+  { url: "https://www.footlocker.ca/en/product/jordan-retro-5-mens/4100636.html" },
+];
 
-const secondItemData = {
-  title: "Jordan Retro 5",
-  currentPrice: 255,
-  url: "https://www.footlocker.ca/en/product/jordan-retro-5-mens/4100636.html",
-  imageURL: "https://images.footlocker.com/is/image/EBFL2/4100636_a1?wid=630&hei=630&fmt=png-alpha",
-};
-
-let itemId: string;
-let secondItemId: string;
+let items: IItem[] = [];
 
 describe("Item Endpoints", () => {
   beforeAll(async () => {
@@ -43,64 +28,75 @@ describe("Item Endpoints", () => {
   });
 
   test("create an item", async () => {
-    const res = await request.post("/api/i/").send(itemData);
-    expect(res.statusCode).toEqual(201);
+    const res = await request.post("/api/i/").send(itemURLs[0]);
+    expect(res.status).toEqual(201);
     expect(res.body).toHaveProperty("payload");
     const payload = res.body.payload;
 
-    expect(payload.title).toEqual(itemData.title);
-    expect(payload.currentPrice).toEqual(itemData.currentPrice);
-    expect(payload.url).toEqual(itemData.url);
-    expect(payload.imageURL).toEqual(itemData.imageURL);
+    expect(payload.title).toBeTruthy();
+    expect(payload.currentPrice).toBeTruthy();
+    expect(payload.url).toEqual(itemURLs[0].url);
+    expect(payload.imageURL).toBeTruthy();
 
-    itemId = payload._id;
+    items.push(payload);
   });
 
   test("add duplicate item", async () => {
-    const res = await request.post("/api/i/").send(itemData);
-    expect(res.statusCode).toEqual(422);
+    const res = await request.post("/api/i/").send(itemURLs[0]);
+    expect(res.status).toEqual(422);
   });
 
   test("get 1 item", async () => {
-    const res = await request.get(`/api/i/${itemId}`);
+    const res = await request.get(`/api/i/${items[0]._id}`);
     const payload = res.body.payload;
 
-    expect(payload.title).toEqual(itemData.title);
-    expect(payload.currentPrice).toEqual(itemData.currentPrice);
-    expect(payload.url).toEqual(itemData.url);
-    expect(payload.imageURL).toEqual(itemData.imageURL);
+    expect(payload.title).toEqual(items[0].title);
+    expect(payload.currentPrice).toEqual(items[0].currentPrice);
+    expect(payload.url).toEqual(items[0].url);
+    expect(payload.imageURL).toEqual(items[0].imageURL);
   });
 
   test("create second item", async () => {
-    const res = await request.post("/api/i/").send(secondItemData);
-    expect(res.statusCode).toEqual(200);
+    const res = await request.post("/api/i/").send(itemURLs[1]);
+    expect(res.status).toEqual(201);
     expect(res.body).toHaveProperty("payload");
     const payload = res.body.payload;
 
-    expect(payload.title).toEqual(secondItemData.title);
-    expect(payload.currentPrice).toEqual(secondItemData.currentPrice);
-    expect(payload.url).toEqual(secondItemData.url);
-    expect(payload.imageURL).toEqual(secondItemData.imageURL);
+    expect(payload.title).toBeTruthy();
+    expect(payload.currentPrice).toBeTruthy();
+    expect(payload.url).toEqual(itemURLs[1].url);
+    expect(payload.imageURL).toBeTruthy();
 
-    secondItemId = payload._id;
+    items.push(payload);
   });
 
   test("get all items", async () => {
     const res = await request.get("/api/i/");
-    expect(res.statusCode).toEqual(200);
+    expect(res.status).toEqual(200);
     expect(res.body).toHaveProperty("payload");
 
     const payload = res.body.payload;
     expect(payload.length).toEqual(2);
-    expect(payload[0].title).toEqual(updatedItemData.title);
-    expect(payload[1].title).toEqual(secondItemData.title);
+    expect(payload[0].title).toEqual(items[0].title);
+    expect(payload[1].title).toEqual(items[1].title);
+  });
+
+  test("get paginated items", async () => {
+    const res = await request.get("/api/i/limit/1/page/2");
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty("payload");
+
+    const payload = res.body.payload;
+    expect(payload.length).toEqual(1);
+    expect(payload[0].title).toEqual(items[1].title);
   });
 
   test("delete items", async () => {
-    let res = await request.delete(`/api/i/${itemId}`);
-    expect(res.statusCode).toEqual(200);
+    // convert to loop
+    let res = await request.delete(`/api/i/${items[0]._id}`);
+    expect(res.status).toEqual(200);
 
-    res = await request.delete(`/api/i/${secondItemId}`);
-    expect(res.statusCode).toEqual(200);
+    res = await request.delete(`/api/i/${items[1]._id}`);
+    expect(res.status).toEqual(200);
   });
 });
