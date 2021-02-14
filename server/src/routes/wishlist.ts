70b998e00ce699634a20c4938ba29afa80aff01a
Amandeep Rehal <arehal4@gmail.com>
2021-02-13 16:22:24 -0800

    feat: implemented endpoints for getting wishlists

--- server/src/routes/wishlist.ts
+++ server/src/routes/wishlist.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createWishlist } from "../controllers/wishlist";
+import { createWishlist, getOwnWishlists, getSharedWishlists } from "../controllers/wishlist";
 
 const router = Router();
 
 router.post("/", createWishlist);
+router.get("/", getOwnWishlists);
+router.get("/shared", getSharedWishlists);
 
 export default router;
