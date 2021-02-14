e98c41483e1b04634048a7bb14bb79834d9aece5
Amandeep Rehal <arehal4@gmail.com>
2021-02-13 16:40:13 -0800

    feat: implemented delete wishlist endpoint

--- server/src/routes/wishlist.ts
+++ server/src/routes/wishlist.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { createWishlist, getOwnWishlists, getSharedWishlists } from "../controllers/wishlist";
+import { deleteModel } from "mongoose";
+import {
+  createWishlist,
+  deleteWishlist,
+  getOwnWishlists,
+  getSharedWishlists,
+} from "../controllers/wishlist";
 
 const router = Router();
 
 router.post("/", createWishlist);
 router.get("/", getOwnWishlists);
 router.get("/shared", getSharedWishlists);
+router.delete("/", deleteWishlist);
 
 export default router;
