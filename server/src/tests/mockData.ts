a0e5d8a2020544944dc76e54433a8599006edc04
Amandeep Rehal <arehal4@gmail.com>
2021-02-18 14:30:44 -0800

    feat: created mock data for tests

---
+++ server/src/tests/mockData.ts
@@ -0,0 +1,30 @@
+export const itemData = [
+  {
+    title: "Sony Over-Ear Noise Cancelling Bluetooth Headphones (WH-1000XM4/B) - Black",
+    currentPrice: 499.99,
+    url: "https://www.bestbuy.ca/en-ca/product/14777258",
+    imageURL:
+      "https://multimedia.bbycastatic.ca/multimedia/products/500x500/147/14777/14777258.jpg",
+  },
+  {
+    title: "Jordan Retro 5",
+    currentPrice: 255,
+    url: "https://www.footlocker.ca/en/product/jordan-retro-5-mens/4100636.html",
+    imageURL:
+      "https://images.footlocker.com/is/image/EBFL2/4100636_a1?wid=630&hei=630&fmt=png-alpha",
+  },
+  {
+    title: "CORSAIR HS60 Pro Surround Gaming Headset, Carbon (CA-9011213-NA)",
+    currentPrice: 89.99,
+    url:
+      "https://www.canadacomputers.com/product_info.php?cPath=1211_1215&item_id=140937&sid=smlpqqro78suo5ef3h6bj9v1b3",
+    imageURL: "https://ccimg.canadacomputers.com/Products/600x600/846/848/140937/84408.jpg",
+  },
+];
+
+export const userData = [
+  { email: `test1@test.ca`, firstName: `test1`, lastName: "sparklist" },
+  { email: `test2@test.ca`, firstName: `test2`, lastName: "sparklist" },
+  { email: `test3@test.ca`, firstName: `test3`, lastName: "sparklist" },
+  { email: `test4@test.ca`, firstName: `test4`, lastName: "sparklist" },
+];
