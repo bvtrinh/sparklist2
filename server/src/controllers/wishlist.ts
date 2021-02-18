de456a88e88750c6b8083b6bd02436120e1a00db
Amandeep Rehal <arehal4@gmail.com>
2021-02-18 14:30:03 -0800

    refactor: update returns for create wishlist endpoint

--- server/src/controllers/wishlist.ts
+++ server/src/controllers/wishlist.ts
@@ -8,11 +8,16 @@ export const createWishlist: RequestHandler = async (req, res) => {
   try {
     // check if owner has another wishlist with same name
     if (!(await validateWishlistName(wishlistData))) {
-      throw new Error("Wishlist with that name already exists, please try again.");
+      return res.status(422).json({
+        message: "Wishlist with that name already exists, please try again.",
+        error: true,
+      });
     }
     const newWishlist: IWishlist = new Wishlist(wishlistData);
     await newWishlist.save();
-    return res.status(200).json({ message: "Created the Item.", error: false });
+    return res
+      .status(201)
+      .json({ payload: newWishlist, message: "Created the Wishlist.", error: false });
   } catch (err) {
     return res.status(500).json({
       payload: err.error,
