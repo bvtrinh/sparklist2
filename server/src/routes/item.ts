909c8fb20667a70ae0ea093739daa88689e99934
Amandeep Rehal <arehal4@gmail.com>
2021-03-03 16:55:50 -0800

    feat: add auth middleware to routes

--- server/src/routes/item.ts
+++ server/src/routes/item.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createItem, deleteItem, getAllItems, getOneItem } from "../controllers/item";
 import { itemURLValidation } from "../middleware/validation";
+import { isAuthenticated } from "../middleware/auth";
 
 const router = Router();
 
-router.post("/", itemURLValidation, createItem);
-router.get("/limit/:limit/page/:page", getAllItems);
-router.get("/:id", getOneItem);
-router.delete("/:id", deleteItem);
+router.post("/", isAuthenticated, itemURLValidation, createItem);
+router.get("/limit/:limit/page/:page", isAuthenticated, getAllItems);
+router.get("/:id", isAuthenticated, getOneItem);
+router.delete("/:id", isAuthenticated, deleteItem);
 
 export default router;
