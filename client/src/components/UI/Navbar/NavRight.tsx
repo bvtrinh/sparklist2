888a10f4b47752c52ae7c9cd7acc9b6774e19d1f
Tyler Trinh <tylervtrinh@gmail.com>
2021-03-17 22:35:40 -0700

    feat: setup user links in NavRight

--- client/src/components/UI/Navbar/NavRight.tsx
+++ client/src/components/UI/Navbar/NavRight.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
 import {
   Flex,
   Menu,
@@ -9,25 +10,27 @@ import {
   MenuDivider,
   Avatar,
 } from "@chakra-ui/react";
-import { NavLink } from "./NavLink";
+import { UserContext } from "../../../helpers/UserContext";
 
 export const NavRight = () => {
   return (
-    <Flex alignItems={"center"}>
-      <Menu>
-        <MenuButton as={Button} color="white" variant={"link"} cursor={"pointer"}>
-          <Avatar size={"sm"} src="/images/profile-placeholder.png" />{" "}
-        </MenuButton>
-        <MenuList color="black">
-          <MenuItem>
-            <NavLink to="#">Profile</NavLink>
-          </MenuItem>
-          <MenuDivider />
-          <MenuItem>
-            <NavLink to="#">Logout</NavLink>
-          </MenuItem>
-        </MenuList>
-      </Menu>
-    </Flex>
+    <UserContext.Consumer>
+      {(context) => (
+        <Flex alignItems={"center"}>
+          <Menu>
+            <MenuButton as={Button} color="white" variant={"link"} cursor={"pointer"}>
+              <Avatar size={"sm"} src="/images/profile-placeholder.png" />{" "}
+            </MenuButton>
+            <MenuList color="black">
+              <MenuItem as={RouterLink} to="/profile">
+                Profile
+              </MenuItem>
+              <MenuDivider />
+              <MenuItem onClick={context.logoutHandler}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      )}
+    </UserContext.Consumer>
   );
 };
