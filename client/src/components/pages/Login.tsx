bdcf0ce4c42c8563db00d6448e0016388e234d50
Tyler Trinh <tylervtrinh@gmail.com>
2021-03-19 00:07:50 -0700

    refactor: use heading over text and remove padding from login page

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { VStack, Text } from "@chakra-ui/react";
+import { VStack, Heading } from "@chakra-ui/react";
 import { GoogleButton } from "../UI/GoogleButton";
 import { TwitterButton } from "../UI/TwitterButton";
 
 export const Login: React.FC = () => {
   return (
-    <VStack pt={5} align="start">
-      <Text fontSize="3xl">Login</Text>
+    <VStack align="start">
+      <Heading>Login</Heading>
       <GoogleButton />
       <TwitterButton />
     </VStack>
