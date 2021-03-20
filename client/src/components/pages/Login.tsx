7090bb2a0c48ec2f14c36904fe2cf026d2d2919e
Tyler Trinh <tylervtrinh@gmail.com>
2021-03-20 01:33:03 -0700

    feat: if user is authed and on login page redirect to /

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { VStack, Heading } from "@chakra-ui/react";
 import { GoogleButton } from "../UI/GoogleButton";
 import { TwitterButton } from "../UI/TwitterButton";
+import { checkAuth } from "../../helpers/authSession";
 
 export const Login: React.FC = () => {
+  const history = useHistory();
+  if (checkAuth()) history.push("/");
   return (
     <VStack align="start">
       <Heading>Login</Heading>
